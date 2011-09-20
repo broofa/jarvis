79968c8d328a94780a25a1803d58b37cd625d802
Robert Kieffer <robert@broofa.com>
2011-09-20 08:41:14 -0700

    Simpler code for search plugin. Started a webdev module for dev resources

--- plugins/search.js
+++ plugins/search.js
@@ -4,59 +4,18 @@ commands.info = {
   author: 'Jarvis'
 };
 
-var search = {
-  // TODO: This data structure should be modeled (or read directly from?) the
-  // OpenSearch schema
-  ENGINES: {
-    amazon: {
-      name: 'Amazon',
-      url: 'http://www.amazon.com/s?field-keywords=%s'
-    },
-    google: {
-      name: 'Google',
-      url: 'http://www.google.com/search?q=%s'
-    },
-    bing: {
-      name: 'Bing',
-      url: 'http://www.bing.com/search?setmkt=en-US&q=%s'
-    },
-    wikipedia: {
-      name: 'Wikipedia',
-      url: 'http://en.wikipedia.org/wiki/%s'
-    },
-    yahoo: {
-      name: 'Yahoo!',
-      url: 'http://search.yahoo.com/search?ei={inputEncoding}&fr=crmas&p=%s'
-    }
-  },
-
-  DEFAULT_ENGINE: 'google',
-
-  getQueryURL: function(engine, q) {
-    // Handle jarvis shortcuts
-    // TODO: Move this into a separate command
-    if (/^(jarvis|J)$/.test(q)) {
-      return '/';
-    }
-
-    // Get engine info
-    engine = this.ENGINES[engine] || this.ENGINES[this.DEFAULT_ENGINE];
-
-    var url = engine.url;
-    return url.replace(/%s/, q);
-  }
-}
-
-// Add search engine options
-Object.keys(search.ENGINES).sort().forEach(function(k) {
-  var engine = search.ENGINES[k];
-  var phrase = k.substr(0,2).toLowerCase();
+function shortcut(phrase, doc, url) {
   commands.add({
-    doc: 'Search ' + engine.name + ' (e.g. "' + phrase + ' camera")',
+    doc: doc,
     phrase: phrase,
     action: function(query) {
-      query = query.replace(this.match, '');
-      return search.getQueryURL(k, query);
+      return url.replace(/\$1/, RegExp.$1);
     }
   });
-});
+}
+
+shortcut('am', 'Search Amazon', 'http://www.amazon.com/s?field-keywords=$1');
+shortcut('bi', 'Search Bing', 'http://www.bing.com/search?q=$1');
+shortcut('go', 'Search Google', 'http://www.google.com/search?q=$1');
+shortcut('wi', 'Search Wikipedia', 'http://en.wikipedia.org/wiki/$1');
+shortcut('ya', 'Search Yahoo!', 'http://search.yahoo.com/search?p=$1');
---
+++ plugins/webdev.js
@@ -0,0 +1,23 @@
+commands.info = {
+  name: 'Web Developers',
+  doc: 'Resources for web application development',
+  author: 'Robert Kieffer <robert@broofa.com>'
+};
+
+commands.add({
+  doc: 'github.com',
+  phrase: 'gh',
+  action: 'http://github.com'
+});
+
+commands.add({
+  doc: 'stackoverflow.com',
+  phrase: 'so',
+  action: 'http://stackoverflow.com'
+});
+
+commands.add({
+  doc: 'gitref.com',
+  phrase: ['gitref'],
+  action: 'http://gitref.com'
+});
